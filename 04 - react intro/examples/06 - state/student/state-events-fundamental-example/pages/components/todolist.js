80680d0633113358ad5a639ed5c982c175d7dc0b
OHart-NAIT <ohart1@nait.ca>
2026-03-04 10:11:41 -0700

    Made changes to better match instructor version of example

--- 04 - react intro/examples/06 - state/student/state-events-fundamental-example/pages/components/todolist.js
+++ 04 - react intro/examples/06 - state/student/state-events-fundamental-example/pages/components/todolist.js
@@ -1,3 +1,7 @@
+// State import
+import { useState } from "react";
+
+// MUI Components
 import { Box } from "@mui/material";
 import { Button } from "@mui/material";
 import { Grid } from "@mui/material";
@@ -6,7 +10,20 @@ import { Typography } from "@mui/material";
 
 export default function ToDoList() {
 
+    /* [stateVariable, stateVariableSetter] = useState(defaultValue)
+
+      [variable, function] -> think [noun, verb]
+
+      The setter is the *only* thing allowed to change the value of the state variable.
+
+      When the setter function fires (i.e. data in the state variable changes), the component
+      automatically re-renders.
+  */
+    const [todoText, setTodoText] = useState("")
+
     const onTodoTextChange = (event) => {
+        // Call the state variable's setter with a new value to write to that variable.
+        setTodoText(event.target.value)
         console.log(event.target.value)
     }
 
@@ -17,7 +34,9 @@ export default function ToDoList() {
     return <Box sx={{ flexGrow: 1}}>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
-                    <Typography variant="h2" component="h2">Our ToDo List</Typography>
+                    <Typography variant="h2" component="h2">
+                        Our ToDo List
+                    </Typography>
                 </Grid>
 
                 <Grid item xs ={10}>
@@ -27,13 +46,18 @@ export default function ToDoList() {
                         variant="standard"
                         sx={{width: '100%'}}
                         onChange={onTodoTextChange}
+                        value={todoText}
                     />
                 </Grid>
                 
                 <Grid item xs ={2}>
-                    <Button variant="contained" onClick={onAddTodoClick}>Add Todo</Button>
+                    <Button 
+                        variant="contained" 
+                        onClick={onAddTodoClick}
+                    >Add Todo</Button>
                 </Grid>
                 
+                Our todo text is: {todoText}
 
             </Grid>
         </Box>
